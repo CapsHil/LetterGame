16777006e914ca0f1ede43e6e9303bbf3b2a5b49
Pierre RABY <Zenika@Pierres-MacBook-Pro.local>
2017-03-03 10:31:56 +0100

    Improve Dictionnary class

--- LetterGame/src/main/java/fr/esiea/raby_ambassa/dictionary/Dictionnary.java
+++ LetterGame/src/main/java/fr/esiea/raby_ambassa/dictionary/Dictionnary.java
@@ -1,22 +1,29 @@
 package fr.esiea.raby_ambassa.dictionary;
 
+import fr.esiea.raby_ambassa.utils.Log;
+import fr.esiea.raby_ambassa.utils.Parser;
+import fr.esiea.raby_ambassa.word.Word;
+
 import java.util.ArrayList;
 
 public class Dictionnary implements IDictionary{
 
 	private static Dictionnary instance = new Dictionnary();
+	private ArrayList<Word> dictionnary;
 	
 	private Dictionnary() {
-		ArrayList<Word> parsedDictionnary = Parser.parse("src/main/resources/dico.txt");
-		Log.verbose(parsedDictionnary.toString());
+		dictionnary = Parser.parse("src/main/resources/dico.txt");
+		Log.verbose(dictionnary.toString());
 	}
 	
 	public static Dictionnary getInstance() {
 		return instance;
 	}
 	
-	public boolean isWord(String word) {
-		// TODO Auto-generated method stub
+	public boolean isWord(String existingWord) {
+		for (Word word : dictionnary)
+			if(word.getWord().toUpperCase().equals(existingWord.toUpperCase()))
+				return true;
 		return false;
 	}
 	
--- LetterGame/src/main/java/fr/esiea/raby_ambassa/dictionary/IDictionary.java
+++ LetterGame/src/main/java/fr/esiea/raby_ambassa/dictionary/IDictionary.java
@@ -1,5 +1,7 @@
 package fr.esiea.raby_ambassa.dictionary;
 
+import fr.esiea.raby_ambassa.word.Word;
+
 /**
  * Created on 16/01/17.
  */
