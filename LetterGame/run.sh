73a19fe2de1a37ce7ae0d577987a4a3d764d031a
Pierre RABY <Zenika@Pierres-MacBook-Pro.local>
2017-03-03 10:15:06 +0100

    Update pom.xml & create run script

--- LetterGame/run.sh
+++ LetterGame/run.sh
@@ -1,1 +1,3 @@
-mvn package && java -cp target/LetterGame-0.0.0-SNAPSHOT.jar fr.esiea.raby_ambassa.dictionary.Application
+mvn package 
+clear
+java -cp target/LetterGame-0.0.0-SNAPSHOT.jar fr.esiea.raby_ambassa.Application
